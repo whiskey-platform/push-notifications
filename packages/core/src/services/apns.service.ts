d12a80468edbb9751ef42a36249f73c72bc96894
Matthew Wyskiel <developer@mattwyskiel.com>
2023-04-23 16:25:56 -0400

    fix APNS service

--- packages/core/src/services/apns.service.ts
+++ packages/core/src/services/apns.service.ts
@@ -14,53 +14,68 @@ const defaultConfig: APNSConfig = {
 
 @Service()
 export class APNSService {
-  public async sendNotification(body: NotificationBody, deviceToken: string, config?: APNSConfig) {
-    logger.info('Begin sending notification to APNS', { body, deviceToken, inputConfig: config });
-    const compiledConfig: APNSConfig = {
-      ...defaultConfig,
-      ...config,
-    };
-    const token = sign(
-      {
-        iss: Config.APPLE_TEAM_ID,
-        iat: Date.now(),
-      },
-      Config.PUSH_KEY,
-      {
-        header: {
-          alg: 'ES256',
-          kid: Config.PUSH_KEY_ID,
+  public async sendNotification(
+    body: NotificationBody,
+    deviceToken: string,
+    config?: APNSConfig
+  ): Promise<string> {
+    return new Promise((resolve, reject) => {
+      logger.info('Begin sending notification to APNS', { body, deviceToken, inputConfig: config });
+      const compiledConfig: APNSConfig = {
+        ...defaultConfig,
+        ...config,
+      };
+      const token = sign(
+        {
+          iss: Config.APPLE_TEAM_ID,
+          iat: Date.now(),
         },
+        Config.PUSH_KEY,
+        {
+          header: {
+            alg: 'ES256',
+            kid: Config.PUSH_KEY_ID,
+          },
+        }
+      );
+      const client = connect(Config.APNS_BASE_URL);
+      logger.debug(`Connected to ${Config.APNS_BASE_URL}`);
+      client.on('error', err => {
+        logger.error('Error sending notification to APNS', { error: err });
+        client.destroy();
+        reject(err);
+      });
+      const headers: Record<string, any> = {
+        ':method': 'POST',
+        'apns-topic': Config.IOS_APP_ID,
+        ':scheme': 'https',
+        ':path': `/3/device/${deviceToken}`,
+        authorization: `bearer ${token}`,
+        'apns-push-type': compiledConfig.pushType,
+        'apns-expiration': compiledConfig.expiration,
+        'apns-priority': compiledConfig.priority,
+      };
+      if (compiledConfig.collapseId) {
+        headers['apns-collapse-id'] = compiledConfig.collapseId;
       }
-    );
-    const client = connect(Config.APNS_BASE_URL);
-    logger.debug(`Connected to ${Config.APNS_BASE_URL}`);
-    client.on('error', err => logger.error('Error connecting to APNS', { error: err }));
-    const headers: Record<string, any> = {
-      ':method': 'POST',
-      'apns-topic': Config.IOS_APP_ID,
-      ':scheme': 'https',
-      ':path': `/3/device/${deviceToken}`,
-      authorization: `bearer ${token}`,
-      'apns-push-type': compiledConfig.pushType,
-      'apns-expiration': compiledConfig.expiration,
-      'apns-priority': compiledConfig.priority,
-    };
-    if (compiledConfig.collapseId) {
-      headers['apns-collapse-id'] = compiledConfig.collapseId;
-    }
-    logger.debug('Sending headers to APNS', { headers });
-    const request = client.request(headers);
+      logger.debug('Sending headers to APNS', { headers });
+      const request = client.request(headers);
 
-    request.on('response', (headers, flags) => {
-      logger.debug('Received response from APNS', { headers, flags });
-    });
+      request.on('response', (headers, flags) => {
+        logger.debug('Received response from APNS', { headers, flags });
+      });
 
-    request.setEncoding('utf8');
-    let data = '';
-    request.on('data', chunk => {
-      data += chunk;
+      request.setEncoding('utf8');
+      let data = '';
+      request.on('data', chunk => {
+        data += chunk;
+      });
+      request.write(JSON.stringify(body));
+      request.on('end', () => {
+        logger.info('Successfully sent notification to APNS', { data });
+        client.destroy();
+        resolve(data);
+      });
     });
-    request.write(JSON.stringify(body));
   }
 }
