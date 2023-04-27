f1acf6397e51e1703681e8d37db01d97e8de010e
Matthew Wyskiel <developer@mattwyskiel.com>
2023-04-27 17:19:36 -0400

    Don't duplicate device tokens for a user

--- packages/api/src/functions/add-device-token.ts
+++ packages/api/src/functions/add-device-token.ts
@@ -14,33 +14,36 @@ import {
 
 const addDeviceToken: APIGatewayJSONBodyEventHandler<PostDeviceTokensRequestBody> = async event => {
   logger.info('Begin Add Device Token request');
-  const count = (
-    await db
-      .selectFrom('device_tokens')
-      .select('index')
-      .where('user_id', '=', parseInt(event.headers['x-user-id']!))
-      .execute()
-  ).length;
-  logger.info(`User ${event.headers['x-user-id']} has ${count} device tokens`);
-  try {
-    logger.info('Saving device token', {
-      userId: event.headers['x-user-id'],
-      deviceToken: event.body.token,
-    });
-    await db
-      .insertInto('device_tokens')
-      .values({
-        user_id: parseInt(event.headers['x-user-id']!),
-        index: count,
-        device_token: event.body.token,
-      })
-      .execute();
-    logger.info('Successfully saved device token to database');
-    return json({ success: true });
-  } catch (error) {
-    logger.error('Error seving device token to DB', error);
-    return json({ success: false });
+  const tokens = await db
+    .selectFrom('device_tokens')
+    .select(['device_token'])
+    .where('user_id', '=', parseInt(event.headers['x-user-id']!))
+    .execute();
+  if (!tokens.map(v => v.device_token).includes(event.body.token)) {
+    // no duplicates
+    const count = tokens.length;
+    logger.info(`User ${event.headers['x-user-id']} has ${count} device tokens`);
+    try {
+      logger.info('Saving device token', {
+        userId: event.headers['x-user-id'],
+        deviceToken: event.body.token,
+      });
+      await db
+        .insertInto('device_tokens')
+        .values({
+          user_id: parseInt(event.headers['x-user-id']!),
+          index: count,
+          device_token: event.body.token,
+        })
+        .execute();
+      logger.info('Successfully saved device token to database');
+      return json({ success: true });
+    } catch (error) {
+      logger.error('Error seving device token to DB', error);
+      return json({ success: false });
+    }
   }
+  return json({ success: true });
 };
 
 export const handler = middy(addDeviceToken)
